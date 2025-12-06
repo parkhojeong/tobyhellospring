890e822cad6a8361587e410edcfcbc0d769925b1
parkhojeong <parkhj062@gmail.com>
2025-12-07 03:56:43 +0900

    PaymentService impl 2

--- src/main/java/tobyspring/tobyhellospring/PaymentService.java
+++ src/main/java/tobyspring/tobyhellospring/PaymentService.java
@@ -1,13 +1,20 @@
 package tobyspring.tobyhellospring;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class PaymentService {
-    public Payment prepare() {
-        return new Payment();
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+        // get exchange rate
+        // calculate amount
+        // cal validUntil
+        return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO,
+                LocalDateTime.now());
     }
 
     public static void main(String[] args) {
         PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println("payment = " + payment);
     }
 }
