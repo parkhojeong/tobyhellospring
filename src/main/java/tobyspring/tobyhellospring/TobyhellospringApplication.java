47d8732927fb7bf2546c2d073b39f0b8afefb787
parkhojeong <parkhj062@gmail.com>
2025-12-07 03:09:47 +0900

    Initial commit

---
+++ src/main/java/tobyspring/tobyhellospring/TobyhellospringApplication.java
@@ -0,0 +1,13 @@
+package tobyspring.tobyhellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobyhellospringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobyhellospringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tobyspring/tobyhellospring/TobyhellospringApplicationTests.java
@@ -0,0 +1,13 @@
+package tobyspring.tobyhellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyhellospringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
