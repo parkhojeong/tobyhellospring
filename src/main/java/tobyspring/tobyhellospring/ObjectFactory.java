2a796893e874e87b090f158cc5a8a787f0f30e70
parkhojeong <parkhj062@gmail.com>
2025-12-08 02:11:07 +0900

    refactor: Client
    use ObjectFactory

---
+++ src/main/java/tobyspring/tobyhellospring/ObjectFactory.java
@@ -0,0 +1,11 @@
+package tobyspring.tobyhellospring;
+
+public class ObjectFactory {
+    public PaymentService paymentService() {
+        return new PaymentService(exRateProvider());
+    }
+
+    public ExRateProvider exRateProvider() {
+        return new WebApiExRateProvider();
+    }
+}
