429fe82f356676f677830d6637a30bc50fbec9e8
parkhojeong <parkhj062@gmail.com>
2025-12-08 01:52:35 +0900

    refactor: PaymentService
    use interface

--- src/main/java/tobyspring/tobyhellospring/SimpleExRateProvider.java
+++ src/main/java/tobyspring/tobyhellospring/SimpleExRateProvider.java
@@ -3,8 +3,8 @@ package tobyspring.tobyhellospring;
 import java.io.IOException;
 import java.math.BigDecimal;
 
-public class SimpleExRateProvider {
-    BigDecimal getExRate(String currency) throws IOException {
+public class SimpleExRateProvider implements ExRateProvider{
+    public BigDecimal getExRate(String currency) throws IOException {
         if(currency.equals("USD")) return BigDecimal.valueOf(1000);
 
         throw new IllegalArgumentException("지원되지 않는 통화입니다");
