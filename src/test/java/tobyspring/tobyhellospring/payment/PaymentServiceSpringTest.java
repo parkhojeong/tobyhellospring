1453d08675d296f75be2b2db961d84eaf9e24cab
hj <parkhj062@gmail.com>
2025-12-09 00:35:07 +0900

    PaymentServiceSpringTest with spring DI

--- src/test/java/tobyspring/tobyhellospring/payment/PaymentServiceSpringTest.java
+++ src/test/java/tobyspring/tobyhellospring/payment/PaymentServiceSpringTest.java
@@ -1,8 +1,12 @@
 package tobyspring.tobyhellospring.payment;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -10,11 +14,15 @@ import java.math.BigDecimal;
 import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestObjectFactory.class)
 class PaymentServiceSpringTest {
+
+    @Autowired
+    private BeanFactory beanFactory;
 
     @Test
     void convertedAmount() throws IOException {
-        BeanFactory beanFactory = new AnnotationConfigApplicationContext(TestObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
