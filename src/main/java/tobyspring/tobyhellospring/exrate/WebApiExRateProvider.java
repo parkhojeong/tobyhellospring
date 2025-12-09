64665b7a803c50136c0b144ee20acacdfeca6ff4
hj <parkhj062@gmail.com>
2025-12-09 22:16:37 +0900

    WebApiExRateProvider refactoring
    extract code to HttpClientApiExecutor class

--- src/main/java/tobyspring/tobyhellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/tobyhellospring/exrate/WebApiExRateProvider.java
@@ -2,13 +2,9 @@ package tobyspring.tobyhellospring.exrate;
 
 import tobyspring.tobyhellospring.api.ApiTemplate;
 import tobyspring.tobyhellospring.api.ErApiExRateExtractor;
-import tobyspring.tobyhellospring.api.SimpleApiExecutor;
 import tobyspring.tobyhellospring.payment.ExRateProvider;
 
 import java.math.BigDecimal;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 public class WebApiExRateProvider implements ExRateProvider {
     ApiTemplate apiTemplate = new ApiTemplate();
@@ -17,17 +13,6 @@ public class WebApiExRateProvider implements ExRateProvider {
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        return apiTemplate.getExRate(url, uri -> {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(uri)
-                    .GET()
-                    .build();
-
-            try(HttpClient client = HttpClient.newBuilder().build()) {
-                return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        } , new ErApiExRateExtractor());
+        return apiTemplate.getExRate(url, new HttpClientApiExecutor() , new ErApiExRateExtractor());
     }
 }
