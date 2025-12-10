aba9f8a18efc79636f679c9530b8b0e2317e5265
hj <parkhj062@gmail.com>
2025-12-10 20:44:33 +0900

    OrderService(Application service)

--- src/main/java/tobyspring/tobyhellospring/DataConfig.java
+++ src/main/java/tobyspring/tobyhellospring/DataConfig.java
@@ -49,8 +49,5 @@ public class DataConfig {
         return new JpaTransactionManager(emf);
     }
 
-    @Bean
-    public OrderRepository orderRepository() {
-        return new OrderRepository();
-    }
+
 }
---
+++ src/main/java/tobyspring/tobyhellospring/OrderClient.java
@@ -0,0 +1,19 @@
+package tobyspring.tobyhellospring;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import tobyspring.tobyhellospring.order.Order;
+import tobyspring.tobyhellospring.order.OrderService;
+
+import java.math.BigDecimal;
+
+public class OrderClient {
+    public static void main(String[] args) {
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(OrderConfig.class);
+        OrderService service = beanFactory.getBean(OrderService.class);
+
+        Order order = service.createOrder("100", BigDecimal.TEN);
+        System.out.println("order = " + order);
+    }
+}
